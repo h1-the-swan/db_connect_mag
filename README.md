8357e6637d516dfd4a68f4a7bf092aa55ebc9675
hi-the-swan <jason.portenoy@gmail.com>
2018-01-24 11:52:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# db_connect_mag
+
+Utilities to connect to custom MySQL (local) database for Microsoft Academic Graph data.
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='db_connect_mag',
+    version='0.1.0',
+    description='Connect to custom (local) MySQL database for Microsoft Academic Graph data',
+    long_description=readme,
+    author='Jason Portenoy',
+    author_email='jason.portenoy@gmail.com',
+    url='https://github.com/h1-the-swan/db_connect_mag.git',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[]
+)
+
